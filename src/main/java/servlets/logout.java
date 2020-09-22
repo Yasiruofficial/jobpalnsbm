98afed0b4e4fd93f59a438c7dccd8be3ed470b8a
Yasiru Swaris <yasiruswaris@gmail.com>
2020-09-22 16:58:32 +0530

    add logout.java servlet

---
+++ src/main/java/servlets/logout.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import util.common;
+
+/**
+ *
+ * @author yasir
+ */
+@WebServlet(name = "logout", urlPatterns = {"/logout"})
+public class logout extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        if(request.getParameter("logout") != null){
+            session.invalidate();
+            response.sendRedirect(common.getHome_servlet());
+        }
+        else{
+            response.sendRedirect(common.getHome_servlet());
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
