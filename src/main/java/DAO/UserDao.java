48026e0508b875222ecd23f89f8595e153b14ea9
imanthikavya <imanthikavya1998@gmail.com>
2020-09-22 15:24:34 +0530

    add isValid to UserDao

--- src/main/java/DAO/UserDao.java
+++ src/main/java/DAO/UserDao.java
@@ -6,6 +6,9 @@
 package DAO;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.servlet.http.HttpSession;
 import util.DBConnection;
 /**
@@ -28,5 +31,30 @@ public class UserDao {
         return userDetails;
         
     }//Get user details from session
+    
+    public final boolean isValid(String userEmail,String password) {
+        
+        try{
+            int rowCount=0;
+            
+            String sql = "SELECT * FROM user WHERE email=? AND password=?";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, userEmail);
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+                
+            while(rs.next()){
+                 rowCount++;
+            }
+               
+           return rowCount == 1 ;
+            
+        }catch(SQLException ex){
+            System.out.println("Exeption By isValidUserLogin : UserDao");
+            System.out.println(ex);
+            return false;
+        }
+        
+    }//Check The Availability Of the user
     
 }
